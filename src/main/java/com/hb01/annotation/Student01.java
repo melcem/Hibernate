9d062777e55d94ad1a461ebab09792524ff1988d
Metin Celik <melcem@gmail.com>
2023-03-21 23:21:51 +0300

    Student01

--- src/main/java/com/hb01/annotation/Student01.java
+++ src/main/java/com/hb01/annotation/Student01.java
@@ -1,4 +1,47 @@
 package com.hb01.annotation;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity // @Entity ile bu sinifin DB de bir tabloya karsilik gelmesini sagliyoruz, student01(tablo adi otomatik)
+@Table(name="t_student01") // (opsiyoneldir) DB de tablo isminin "t_student01" olarak degismesini saglar
 public class Student01 {
+    private int id;
+    private String name;
+    private int grade;
+
+    public int getId() {
+        return id;
+    }
+
+    // Getter-Setter
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Student01{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
