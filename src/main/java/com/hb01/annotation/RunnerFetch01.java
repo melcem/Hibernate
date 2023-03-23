9c3a15de35dbb00584feaa2b57ae49d803aa9d9e
Metin Celik <melcem@gmail.com>
2023-03-24 01:21:45 +0300

    RunnerFetch01

---
+++ src/main/java/com/hb01/annotation/RunnerFetch01.java
@@ -0,0 +1,39 @@
+package com.hb01.annotation;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch01 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().
+                configure("hibernate.cfg.xml").addAnnotatedClass(Student01.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+       /*
+            DB den bilgi almak istiyorsam
+                1) get()
+                2) SQL
+                3) HQL
+
+        */
+
+        // !!! 1.Yol : get() ***********************************************
+        Student01 student1 = session.get(Student01.class, 1001);
+
+        System.out.println(student1);
+
+
+
+
+
+        tx.commit();
+
+        session.close();
+        sf.close();
+    }
+}
