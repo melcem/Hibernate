17bc0a027e584075343f41849dbbbdd960a8a325
Metin Celik <melcem@gmail.com>
2023-03-24 00:24:37 +0300

    Student01

--- src/main/java/com/hb01/annotation/Student01.java
+++ src/main/java/com/hb01/annotation/Student01.java
@@ -4,8 +4,8 @@ import javax.persistence.*;
 
 @Entity //!!! @Entity ile bu sinifin DB de bir tabbloya karsilik gelmesini sagliyoruz, student01
 @Table(name="t_student01") //!!! DB de tablo isminin "t_student01" olarak degismesini sagladim
-//Java kodu içinde bu class'a ulaşırken Student01 ile, SQL ile ulaşirken t_student01 ile yazmam lazım
-public class Student01 {
+// java kodu icinde bu classa ulasirken Student01 ile, SQL ile ulasirken t_student01 ile yazmam lazim
+public class Student01 { //student01
 
     @Id // !!! primary-key olusmasini sagliyoruz
     //@Column(name="std_id")
@@ -17,7 +17,7 @@ public class Student01 {
     //@Transient // !!! DB deki tabloda "grade" adinda bir kolon olusmasini engeller
     private int grade;
 
-//    @Lob // !!! large object ile buyuk boyutlu datalar tutulabiliyor
+//    @Lob  // !!! large object ile buyuk boyutlu datalar tutulabiliyor
 //    private byte[] image;
 
     // !!! GETTER - SETTER
