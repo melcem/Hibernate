0627f4b38097a4b6f56c1429286b03c352645cb8
Metin Celik <melcem@gmail.com>
2023-03-24 22:28:34 +0300

    Student03

--- src/main/java/com/hb03/uni_onetoone/Student03.java
+++ src/main/java/com/hb03/uni_onetoone/Student03.java
@@ -14,4 +14,41 @@ public class Student03 {
     private String name;
 
     private int grade;
+
+    //!!! Getter-Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    //!!! toString
+
+    @Override
+    public String toString() {
+        return "Student03{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
