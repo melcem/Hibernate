404c38bb43c100977b0b3b1534b447a6fdee9421
Metin Celik <melcem@gmail.com>
2023-03-26 23:36:41 +0300

    RunnerSave01

---
+++ src/main/java/com/practice01/annotations/RunnerSave01.java
@@ -0,0 +1,33 @@
+package com.practice01.annotations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave01 {
+    public static void main(String[] args) {
+
+        Developer01 dev1=new Developer01(1,"Harry Potter","hp@gmail.com","backend");
+        Developer01 dev2=new Developer01(2,"Shrek","shrek@gmail.com","frontend");
+        Developer01 dev3=new Developer01(3,"Jack Sparrow","jack@gmail.com","mobile");
+        Developer01 dev4=new Developer01(4,null,"null@mail.com","fullstack");
+
+        // default: hibernate.cfg.xml - yazilmasa default degeri cagirir(ismi ayni oldugu surece)
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer01.class);
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(dev1);
+        session.save(dev2);
+        session.save(dev3);
+        session.save(dev4);
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
