3f6d6667ea835d099a41f265662abdd7411800ac
Metin Celik <melcem@gmail.com>
2023-03-29 18:22:34 +0300

    RunnerFetch03

--- src/main/java/com/practice03/onetoone/RunnerFetch03.java
+++ src/main/java/com/practice03/onetoone/RunnerFetch03.java
@@ -13,9 +13,21 @@ public class RunnerFetch03 {
         Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
 
+        //1) id=1 olan developer'in tum bilgilerini getir
         Developer03 dev1 = session.get(Developer03.class,1);
         System.out.println(dev1);
-        System.out.println(dev1.getComputer());
+
+        //2) id=103 olan computer'in tum bilgilerini getir
+        Computer computer = session.get(Computer.class,103);
+        System.out.println(computer);
+
+        //3) id=102 olan computer'i kullanan developer'in tum bilgilerini getir
+        Computer computer1 = session.get(Computer.class,102);
+        System.out.println(computer1.getDeveloper());
+
+        //4) id=3 olan developer'in kullandigi compter'in tum bilgilerini getiriniz
+        Developer03 dev2 = session.get(Developer03.class,3);
+        System.out.println(dev2.getComputer());
 
         tx.commit();
         session.close();
