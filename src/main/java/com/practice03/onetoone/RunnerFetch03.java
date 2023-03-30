15cc8f29cd5fc5a67f50916a362a4dad2d81f474
Metin Celik <melcem@gmail.com>
2023-03-30 18:36:33 +0300

    RunnerFetch03

--- src/main/java/com/practice03/onetoone/RunnerFetch03.java
+++ src/main/java/com/practice03/onetoone/RunnerFetch03.java
@@ -5,6 +5,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RunnerFetch03 {
     public static void main(String[] args) {
         Configuration con = new Configuration().configure("hibernate.cfg.xml").
@@ -25,9 +28,17 @@ public class RunnerFetch03 {
         Computer computer1 = session.get(Computer.class,102);
         System.out.println(computer1.getDeveloper());
 
-        //4) id=3 olan developer'in kullandigi compter'in tum bilgilerini getiriniz
+        //4) id=3 olan developer'in kullandigi computer'in tum bilgilerini getiriniz
         Developer03 dev2 = session.get(Developer03.class,3);
-        System.out.println(dev2.getComputer());
+        System.out.println(dev2.getComputer()); // uni-directional olsaydi computerden dev'e ulasamazdik
+
+        //odev developer'larin ismini ve kullandiklari computer'in markasini yazdiriniz.
+        String hqlQuery = "SELECT d.name, c.brand FROM Developer03 d LEFT JOIN FETCH Computer c on d.id=d.computer.id";
+        System.out.println("------------Homework----------");
+        List<Object[]> resultList = session.createQuery(hqlQuery).getResultList();
+        resultList.forEach(oa->{
+            System.out.println(Arrays.toString(oa));
+        });
 
         tx.commit();
         session.close();
