445ea42d1a9c87ee8d5ff100f325c247858c5eab
Metin Celik <melcem@gmail.com>
2023-03-30 19:33:57 +0300

    Student07

---
+++ src/main/java/com/hb07/bi_onetomany/Student07.java
@@ -0,0 +1,66 @@
+package com.hb07.bi_onetomany;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Student07 {
+
+    @Id
+    private int id;
+
+    @Column(name="student_name",nullable = false)
+    private String name;
+
+    private int grade;
+
+    @OneToMany(mappedBy = "student")
+    private List<Book07> book07List =new ArrayList<>();
+
+    //!!! Getter-Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<Book07> getBook07List() {
+        return book07List;
+    }
+
+    /*public void setBook07List(List<Book07> book07List) {
+        this.book07List = book07List;
+    }*/
+
+    //!!! toString()
+
+    @Override
+    public String toString() {
+        return "Student07{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", book07List=" + book07List +
+                '}';
+    }
+}
