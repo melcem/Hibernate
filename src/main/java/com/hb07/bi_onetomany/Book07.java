3840e6855b5165c2908dcc900efac5c3b502de5c
Metin Celik <melcem@gmail.com>
2023-03-30 19:50:19 +0300

    Book07

--- src/main/java/com/hb07/bi_onetomany/Book07.java
+++ src/main/java/com/hb07/bi_onetomany/Book07.java
@@ -12,4 +12,40 @@ public class Book07 {
     @ManyToOne
     @JoinColumn // student_id
     private Student07 student;
+
+    //!!!Getter-Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student07 getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student07 student) {
+        this.student = student;
+    }
+
+    //!!! toString()
+
+    @Override
+    public String toString() {
+        return "Book07{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
