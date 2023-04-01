128f80956a7e074734000c1be87318e546812986
Metin Celik <melcem@gmail.com>
2023-04-01 20:31:49 +0300

    RunnerFetch08

--- src/main/java/com/hb08/manytomany/RunnerFetch08.java
+++ src/main/java/com/hb08/manytomany/RunnerFetch08.java
@@ -1,11 +1,11 @@
 package com.hb08.manytomany;
 
-import com.hb01.annotation.Student01;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.awt.print.Book;
 import java.util.List;
 
 public class RunnerFetch08 {
@@ -16,9 +16,19 @@ public class RunnerFetch08 {
         Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
 
-        String hqlQuery1 = "FROM Student08";
+       /* //!!! get ile student getir
+        Student08 student1 = session.get(Student08.class,1001);
+        System.out.println(student1);*/
+
+        //!!! get ile book getir
+        Book08 book1 = session.get(Book08.class,101);
+        System.out.println(book1);
+
+        /*String hqlQuery1 = "FROM Student08";
         List<Student08> resultList = session.createQuery(hqlQuery1,Student08.class).getResultList();
-        resultList.forEach(t-> System.out.println(t));
+        resultList.forEach(t-> System.out.println(t));*/
+
+
 
         tx.commit();
         session.close();
