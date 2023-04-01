1d865ad487f840eb4c6603bb82d0abe87689a842
Metin Celik <melcem@gmail.com>
2023-04-01 22:30:21 +0300

    RunnerSave10

---
+++ src/main/java/com/hb10/idgeneration/RunnerSave10.java
@@ -0,0 +1,33 @@
+package com.hb10.idgeneration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave10 {
+    public static void main(String[] args) {
+
+        Student10 student1 = new Student10();
+        student1.setName("Ahmet");
+        student1.setGrade(90);
+
+        Student10 student2 = new Student10();
+        student2.setName("Mahmut");
+        student2.setGrade(80);
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student10.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        //session.save(student1);
+        session.save(student2);
+        tx.commit();
+        session.close();
+        sf.close();
+
+    }
+}
