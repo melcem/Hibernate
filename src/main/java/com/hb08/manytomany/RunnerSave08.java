312945a7bd7722dfa5fb769f507200892d5121af
Metin Celik <melcem@gmail.com>
2023-04-01 20:19:45 +0300

    RunnerSave08

--- src/main/java/com/hb08/manytomany/RunnerSave08.java
+++ src/main/java/com/hb08/manytomany/RunnerSave08.java
@@ -1,7 +1,12 @@
 package com.hb08.manytomany;
 
-import com.hb07.bi_onetomany.Student07;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RunnerSave08 {
     public static void main(String[] args) {
@@ -28,7 +33,41 @@ public class RunnerSave08 {
         book2.setId(102);
         book2.setName("History Book");
 
+        Book08 book3 = new Book08();
+        book3.setId(103);
+        book3.setName("Java Book");
+
+        List<Book08> bookList1 = new ArrayList<>();
+        bookList1.add(book1);
+        bookList1.add(book2);
+        bookList1.add(book3);
+        student1.setBooklist(bookList1);
+
+        List<Book08> bookList2 = new ArrayList<>();
+        bookList2.add(book1);
+        bookList2.add(book2);
+        student2.setBooklist(bookList2);
+
+        List<Book08> bookList3 = new ArrayList<>();
+        bookList3.add(book1);
+        student3.setBooklist(bookList3);
+
         Configuration con = new Configuration().configure("hibernate.cfg.xml").
                 addAnnotatedClass(Student08.class).addAnnotatedClass(Book08.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(student1);
+        session.save(student2);
+        session.save(student3);
+
+        session.save(book1);
+        session.save(book2);
+        session.save(book3);
+
+        tx.commit();
+        session.close();
+        sf.close();
     }
 }
