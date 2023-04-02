dc8d3155ed6d8a59623c41def15566ac775cd8e1
Metin Celik <melcem@gmail.com>
2023-04-02 21:40:36 +0300

    RunnerFetch12

---
+++ src/main/java/com/hb12/caching/RunnerFetch12.java
@@ -0,0 +1,36 @@
+package com.hb12.caching;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch12 {
+    public static void main(String[] args) {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student12.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        System.out.println("Ilk get islemi 1 id'li ogrenci icin: ");
+        Student12 student1 = session.get(Student12.class,1L);
+
+        System.out.println("Ikinci get islemi 1 id'li ogrenci icin: ");
+        Student12 student2 = session.get(Student12.class,1L);
+
+        session.clear();
+
+        tx.commit();
+        session.close();
+
+        Session session2 = sf.openSession();
+        Transaction tx2 = session2.beginTransaction();
+
+        System.out.println("session.close() sonrasi icin get islemi 1 id'li ogrenci icin: ");
+        Student12 student3 = session2.get(Student12.class,1L);
+
+        tx.commit();
+        session2.close();
+        sf.close();
+    }
+}
