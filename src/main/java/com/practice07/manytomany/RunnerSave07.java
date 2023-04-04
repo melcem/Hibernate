3fc91411e099e13ac8f653e52cbd1a9181575192
Metin Celik <melcem@gmail.com>
2023-04-04 15:47:43 +0300

    RunnerSave07

---
+++ src/main/java/com/practice07/manytomany/RunnerSave07.java
@@ -0,0 +1,38 @@
+package com.practice07.manytomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave07 {
+    public static void main(String[] args) {
+        Project project1 = new Project(11L,"CarRental");
+        Project project2 = new Project(22L,"AtmApp");
+
+        Developer07 dev1=new Developer07(1L,"Harry Potter","hp@gmail.com","backend");
+        Developer07 dev2=new Developer07(2L,"Shrek","shrek@gmail.com","frontend");
+        Developer07 dev3=new Developer07(3L,"Jack Sparrow","jack@gmail.com","mobile");
+
+        //iliski sahibi tarafindan set edilmeli( fk eklenecek class tarafindan)
+        project1.getDevelopers().add(dev1);
+        project1.getDevelopers().add(dev2);
+
+        project2.getDevelopers().add(dev1);
+        project2.getDevelopers().add(dev2);
+        project2.getDevelopers().add(dev3);
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer07.class).addAnnotatedClass(Project.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(project1);
+        session.save(project2);
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
