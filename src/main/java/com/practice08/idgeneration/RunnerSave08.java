fdbcb6f049c3dfd6f395cc3dbeeeca5949505b35
Metin Celik <melcem@gmail.com>
2023-04-09 11:43:50 +0300

    RunnerSave08

---
+++ src/main/java/com/practice08/idgeneration/RunnerSave08.java
@@ -0,0 +1,37 @@
+package com.practice08.idgeneration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave08 {
+    public static void main(String[] args) {
+        Developer08 dev1=new Developer08("Harry Potter","hp@gmail.com","backend");
+        Developer08 dev2=new Developer08("Shrek","shrek@gmail.com","frontend");
+        Developer08 dev3=new Developer08("Jack Sparrow","jack@gmail.com","mobile");
+        Developer08 dev4=new Developer08("Richie Rich","rich@gmail.com","mobile");
+        Developer08 dev5=new Developer08("Gandalf","gandalf@gmail.com","mobile");
+
+        Configuration cfg=new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer08.class);
+        SessionFactory sf=cfg.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+
+        //seq=100,101,102,103,104,105 olusturur
+
+        session.save(dev1);
+        session.save(dev2);
+        session.save(dev3);
+//        session.save(dev4);
+//        session.save(dev5);
+
+        // id IDENTITY ile generate edildiginde Java uygulamasi siradaki id'yi bilmez.
+        // DB en son kaldigi degerden devam eder.
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
