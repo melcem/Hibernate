1c7ab9f00331447d66ae8ecccdc7f933d903650c
Metin Celik <melcem@gmail.com>
2023-04-09 12:03:37 +0300

    RunnerFetch08

--- src/main/java/com/practice08/idgeneration/RunnerFetch08.java
+++ src/main/java/com/practice08/idgeneration/RunnerFetch08.java
@@ -1,4 +1,28 @@
 package com.practice08.idgeneration;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 public class RunnerFetch08 {
+    public static void main(String[] args) {
+        Configuration cfg=new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer08.class);
+        SessionFactory sf=cfg.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+
+        //1) ismi Gandalf olan developer'in id bilgisini getir.
+        String hqlQuery = "SELECT d.id FROM Developer08 d WHERE d.name='Gandalf'";
+        List<Object> resultList = session.createQuery(hqlQuery).getResultList();
+        resultList.forEach(System.out::println);
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
 }
