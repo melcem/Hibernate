9c2a05491ff8121645d57eabe6c09292eb931049
Metin Celik <melcem@gmail.com>
2023-04-09 11:11:53 +0300

    RunnerFetch07

--- src/main/java/com/practice07/manytomany/RunnerFetch.java
+++
@@ -1,4 +0,0 @@
-package com.practice07.manytomany;
-
-public class RunnerFetch {
-}
---
+++ src/main/java/com/practice07/manytomany/RunnerFetch07.java
@@ -0,0 +1,24 @@
+package com.practice07.manytomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch07 {
+    public static void main(String[] args) {
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer07.class).addAnnotatedClass(Project.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        //1) id'si 1 olan developer'in calistigi projeleri yazdiriniz.
+        Developer07 dev1 = session.get(Developer07.class,1L);
+        System.out.println(dev1);
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
