153a75bb4d466e07a6e2fd16fe06974186106a3b
Metin Celik <melcem@gmail.com>
2023-04-09 16:01:48 +0300

    RunnerFetch09

--- src/main/java/com/practice09/criteriaapi/RunnerFetch09.java
+++ src/main/java/com/practice09/criteriaapi/RunnerFetch09.java
@@ -12,7 +12,23 @@ public class RunnerFetch09 {
         Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
 
+        //1) Update the name of the developer with 1 to 'John Hopkins'.
+        Developer09 dev1 = session.get(Developer09.class,1L);
+        dev1.setName("John Hopkins");
+        session.update(dev1);
 
+        //2) Update the name of the developer with id 1 to 'Poet Henry Hopkins'.
+        String hql1 = "UPDATE Developer09 d SET d.name='Poet Henry Hopkins' WHERE d.id=1";
+        int numRecord = session.createQuery(hql1).executeUpdate();
+        System.out.println("Number of Record: " +numRecord);
+
+        //3) Set the salary of developers with less than $7000 salary to $7850 using the parameter.
+        int updatedSalary = 7850;
+        int checkSalary = 7000;
+        String hql2 = "UPDATE Developer09 d SET d.salary=:updatedSalary WHERE d.salary<:checkSalary";
+        int numOfRecord = session.createQuery(hql2).setParameter("updatedSalary",updatedSalary).
+                                  setParameter("checkSalary",checkSalary).executeUpdate();
+        System.out.println("Number Of Record: " + numOfRecord);
 
         tx.commit();
         session.close();
